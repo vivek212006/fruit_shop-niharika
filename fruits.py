1d060473c20c64d67ab179d875cb50490a14168d
niharika48 <niharikasiri2006@gmail.com>
2025-07-08 21:14:22 +0530

    given fruits name

---
+++ fruits.py
@@ -0,0 +1,12 @@
+fruit_prices = {
+    "apple": 120,
+    "banana": 40,
+    "orange": 80,
+    "mango": 150,
+    "grapes": 100,
+    "pineapple": 90,
+    "watermelon": 30,
+    "kiwi": 200,
+    "papaya": 60,
+    "pomegranate": 140
+}
